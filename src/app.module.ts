3d2ea4a09672746ec18f630e79b946e983458ebc
Orhun Ozer <ozerorhun@gmail.com>
2024-01-19 23:38:17 +0300

    Update .env file and package dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 
